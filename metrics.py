f676ecbd330e71be3156081c5ffd514ec12bdf6b
MahdiyarTaghavi <taghavi.mahdiyar@gmail.com>
2026-06-19 09:43:23 +0330

    implement r2_score

--- metrics.py
+++ metrics.py
@@ -1,7 +1,12 @@
 import numpy as np
 
 # Regression
-def r2_score(y, y_pred): ...
+def r2_score(y, y_pred):
+    ss_res = np.sum((y - y_pred) ** 2)
+    y_mean = np.mean(y)
+    ss_tot = np.sum((y - y_mean) ** 2)
+    return 1 - (ss_res / ss_tot)
+
 def mse(y, y_pred): ...        # mean squared error
 def rmse(y, y_pred): ...       # root mse
 def mae(y, y_pred): ...        # mean absolute error
